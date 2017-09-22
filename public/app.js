a3df56d4b835519cb87aaa5d9cc25f9b1f68c11f
Josh Schairbaum <josh.schairbaum@rackspace.com>
2017-09-22 16:42:01 -0500

    added a better notification system

--- public/app.js
+++ public/app.js
@@ -7,6 +7,50 @@ $(function() {
         utilsScript: '/vendor/intl-phone/libphonenumber/build/utils.js'
     });
 
+    $.notify.defaults({
+        arrowShow: false,
+        position: 'right',
+        style: 'ctc'
+    });
+
+
+    $.notify.addStyle("ctc", {
+        html: "<div>\n<span data-notify-text></span>\n</div>",
+        classes: {
+            base: {
+                "font-weight": "bold",
+                "padding": "8px 15px 8px 14px",
+                "text-shadow": "0 1px 0 rgba(255, 255, 255, 0.5)",
+                "background-color": "#fcf8e3",
+                "border": "1px solid #fbeed5",
+                "border-radius": "4px",
+                "white-space": "nowrap",
+                "background-repeat": "no-repeat",
+                "background-position": "3px 7px"
+            },
+            error: {
+                "color": "#B94A48",
+                "background-color": "#F2DEDE",
+                "border-color": "#EED3D7"
+            },
+            success: {
+                "color": "#468847",
+                "background-color": "#DFF0D8",
+                "border-color": "#D6E9C6"
+            },
+            info: {
+                "color": "#3A87AD",
+                "background-color": "#D9EDF7",
+                "border-color": "#BCE8F1"
+            },
+            warn: {
+                "color": "#C09853",
+                "background-color": "#FCF8E3",
+                "border-color": "#FBEED5"
+            }
+        }
+    });
+
     // Intercept form submission and submit the form with ajax
     $('#contactForm').on('submit', function(e) {
         // Prevent submit event from bubbling and automatically submitting the
@@ -23,9 +67,16 @@ $(function() {
             }
         }).done(function(data) {
             // The JSON sent back from the server will contain a success message
-            alert(data.message);
+            $('#request-call-btn').notify(data.message,
+                                          {
+                                              className: 'info'
+                                          });
+
         }).fail(function(error) {
-            alert(JSON.stringify(error));
+            $('#request-call-btn').notify(JSON.stringify(error),
+                                          {
+                                              className: 'error'
+                                          });
         });
     });
 });
