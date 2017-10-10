ba27ed796b57b3e346e96e040960f1840697dd10
Josh Schairbaum <josh.schairbaum@rackspace.com>
2017-10-10 13:21:13 -0500

    Customer creates a task instead of a phone call
    For the purposes of task routing, when the customer submits a form, that form
    should respond by creating a task with the necessary attributes and assigned
    to the proper WorkflowSid. I've added the WorkflowSid to the necessary
    configuration.
    
    Since we can associate arbitrary attributes with Tasks, it feels unnecessary
    to store the Call request for record keeping purposes. We can get most of this
    information from the various status callbacks.

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,6 @@ var VoiceResponse = twilio.twiml.VoiceResponse;
 var config = require('../config');
 var token = require('../token');
 
-// Copied from docs, unsure why const vs require
-const uuidv1 = require('uuid/v1');
-
 // Create a Twilio REST API client for authenticated requests to Twilio
 var twilio_client = twilio(config.accountSid, config.authToken);
 
@@ -25,70 +22,6 @@ mongoose.connect(config.mongodbURI, { useMongoClient: true }, function (err, res
     console.log ('Succeeded connected to: ' + config.mongodbURI);
   }
 });
-
-var callSchema = new mongoose.Schema({
-    account: {
-        name: String,
-        number: String
-    },
-    callbackURL: String,
-    contact: {
-        name: String,
-        number: String
-    },
-    phoneNumbers: {
-        agent: String,
-        from: String,
-        to: String
-    },
-    requestReason: String,
-    ticketNumber: String,
-    timestampCreated: { type: Date, default: Date.now },
-    timestampUpdated: { type: Date, default: Date.now },
-    uuid: String
-});
-var Call = mongoose.model('Call', callSchema);
-
-// This represents the exact schema passed from a Twilio callback. I've added
-// SIP attributes, even though I don't think we'll be receiving any. Should
-// errors around missing values pop up, there might be a host of
-// SipHeader_<name> fields that I couldn't add because they're variable.
-// Source: https://www.twilio.com/docs/api/twiml/twilio_request
-var callEventSchema = new mongoose.Schema({
-    Called: String,
-    ToState: String,
-    CallerCountry: String,
-    Direction: String,
-    CallerState: String,
-    ToZip: String,
-    CallSid: String,
-    To: String,
-    CallerZip: String,
-    ToCountry: String,
-    ApiVersion: String,
-    CalledZip: String,
-    CalledCity: String,
-    CallStatus: String,
-    From: String,
-    AccountSid: String,
-    CalledCountry: String,
-    CallerCity: String,
-    Caller: String,
-    FromCountry: String,
-    ToCity: String,
-    FromCity: String,
-    CalledState: String,
-    FromZip: String,
-    FromState: String,
-    ParentCallSid: String,
-    SipDomain: String,
-    SipUsername: String,
-    SipCallId: String,
-    SipSourceIp: String,
-    CallUUID: String,
-    CreatedAt: { type: Date, default: Date.now }
-});
-var CallEvent = mongoose.model('CallEvent', callEventSchema);
 
 // Using {strict: false} makes the model schemaless.
 var workspaceEventSchema = new mongoose.Schema({}, {strict: false});
@@ -122,67 +55,26 @@ module.exports = function(app) {
 
     // Handle an AJAX POST request to place an outbound call
     app.post('/call', function(request, response) {
-        var uuid = uuidv1();
 
-        // This should be the publicly accessible URL for your application
-        // Here, we just use the host for the application making the request,
-        // but you can hard code it or use something different if need be
-        var callbackURL = 'https://' + request.headers.host + '/callbacks/' + encodeURIComponent(uuid);
+        // The contract for the POSTed document can be found in /public/app.js#75
+        // I've refrained from repeating it here, although, it could be
+        // necessary should we want to augment the tasks with something else.
+        var taskAttributes = request.body;
 
-        var call = new Call({
-            account: {
-                name: request.body.accountName,
-                number: request.body.accountNumber
-            },
-            callbackURL: callbackURL,
-            contact: {
-                name: request.body.contactName,
-                number: request.body.contactNumber
-            },
-            phoneNumbers: {
-                agent: config.agentNumber,
-                from: config.twilioNumber,
-                to: request.body.phoneNumber // really the customer
-            },
-            requestReason: request.body.requestReason,
-            ticketNumber: request.body.ticketNumber,
-            uuid: uuid
-        });
-
-        call.save(function (err) {if (err) console.log('Error on Call save!')});
-
-        var twilioCallOptions = {
-            url: call.callbackURL,
-            to: call.phoneNumbers.agent,
-            from: call.phoneNumbers.to,
-            statusCallback: `https://${request.headers.host}/events/voice`,
-            statusCallbackEvent: ['initiated', 'ringing', 'answered', 'completed']
-        };
-
-        // Place an outbound call to the user, using the TwiML instructions
-        // from the /callbacks route
-        console.log("REQ", request.body);
-        console.log("CALL", call);
-        console.log("TCO", twilioCallOptions);
-        console.log(`CALLING AGENT: ${twilioCallOptions.to}`);
-
-        twilio_client.calls.create(twilioCallOptions)
-          .then((message) => {
-              console.log("CALL REQUEST RESPONSE:");
-              console.log(message);
-
-              console.log("SID:");
-              console.log(message.sid);
-
-              // message has a variety of interesting values, but I'm going to
-              // just use the webhooks to handle all the CallEvent tracking.
-              response.send({
-                  message: `Thank you, someone will contact you soon from ${twilioCallOptions.from}.`
-              });
-          }).catch((error) => {
-              console.log(error);
-              response.status(500).send(error);
-          });
+        twilio_client.taskrouter.v1
+            .workspaces(config.workspaceSid)
+            .tasks
+            .create({
+                workflowSid: config.workflowSid,
+                attributes: taskAttributes
+            }).then((message) => {
+                response.send({
+                    message: `Thank you, we will contact you via ${config.twilioNumber}`
+                });
+            }).catch((error) => {
+                console.error(error);
+                response.status(500).send(error);
+           });
     });
 
     // This must come before /callbacks/:uuid to be matched explictly.
@@ -199,33 +91,6 @@ module.exports = function(app) {
         twimlResponse.hangup();
         console.log('TWIML', twimlResponse.toString());
         response.send(twimlResponse.toString());
-    });
-
-    // Return TwiML instuctions for the outbound call
-    app.post('/callbacks/:uuid', function(request, response) {
-        var uuid = request.params.uuid;
-
-        Call.findOne({uuid: uuid}).exec(function(err, call) {
-            if (!err) {
-                var callEvent = new CallEvent(request.body);
-                callEvent.CallUUID = uuid;
-
-                callEvent.save(function (err) {if (err) console.log('error on CallEvent save!')});
-                console.log("CALL", call);
-                var customerNumber = call.phoneNumbers.to;
-                var twimlResponse = new VoiceResponse();
-
-                twimlResponse.say(`Click-To-Call requested; dialing customer`, { voice: 'man' });
-                twimlResponse.dial(customerNumber, {callerId: config.twilioNumber});
-
-                console.log(`CALLING CUSTOMER: ${customerNumber}`);
-                console.log('TWIML', twimlResponse.toString());
-                response.send(twimlResponse.toString());
-            } else {
-                console.log(err);
-                response.status(500).send(err);
-            };
-        });
     });
 
     // Twilio Voice Call Status Change Webhook
