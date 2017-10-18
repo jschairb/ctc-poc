2128cb6cf0e136d4457ce1706d0ff4860c59cb12
Francesco Lazzarino <flazzarino@gmail.com>
2017-10-18 16:54:38 -0500

    adjusting eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,12 @@
 module.exports = {
-    "extends": "airbnb"
+    "extends": "airbnb",
+    "rules": {
+        "indent": ["error", 4],
+        "no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+        "react/jsx-filename-extension": ["error", { "extensions": [".js", ".jsx"] }],
+        "react/jsx-indent": ["error", 4],
+        "react/jsx-indent-props": ["error", 4],
+        "react/jsx-closing-bracket-location": [1, "after-props"],
+    }
+    
 };
